20ec37caa92d14dc2080b55b8305151a21ce3974
Iago Silva <iago.scandido3@gmail.com>
2023-08-30 01:23:48 -0300

    first commit

---
+++ src/main/java/com/iagoscandido/fakeapi/FakeApiApplication.java
@@ -0,0 +1,13 @@
+package com.iagoscandido.fakeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iagoscandido/fakeapi/FakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagoscandido.fakeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
